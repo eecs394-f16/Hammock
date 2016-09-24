723661942b9f26834d61010c8420ce926f1acb22
Agam Gupta <agamgupta2018@u.northwestern.edu>
2016-09-24 11:28:23 -0500

    Added controller feature to read all posts from our databsase

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -1,8 +1,16 @@
 angular.module('SteroidsApplication', [
   'supersonic'
 ])
-.controller('IndexController', function($scope, supersonic) {
+.controller('IndexController', function($scope, $http, supersonic) {
 
   $scope.navbarTitle = "Welcome to Supersonic!";
+  $scope.allposts = {}
 
+  $http.get("www.herokuapp.com/allposts")
+  	.success(data){
+  		$scope.allposts = data
+  	}
+  	.error(data){
+  		console.log("The error is: " + data)
+  	}
 });
