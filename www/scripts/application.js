5f71c4c6f3301b1a9cbc4c5a5c93a7db563c0ec4
Joon Park <JoonPark2017@u.northwestern.edu>
2016-09-25 14:28:01 -0500

    Add different colors to boxes.

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -5,6 +5,20 @@ angular.module('SteroidsApplication', [
 
   $scope.allposts = {};
   $scope.navbarTitle = "Hammock";
+  $scope.boxColors = [
+    "#58B947",
+    "#7FCECD",
+    "#5091CD",
+    "#EDE93B",
+    "#FFC520",
+    "#EF553F",
+    "#008656",
+    "#007FA4",
+    "#0D2D6C",
+    "#D9C826",
+    "#CA7C1B",
+    "#D85820"
+  ];
 
   $http.get("https://tree2hammock.herokuapp.com/getLiveData")
   	.success(function(data) {
