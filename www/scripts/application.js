7f9e65403de669cdfd6ab4c94907ada40c8459c1
Joon Park <JoonPark2017@u.northwestern.edu>
2016-09-25 13:52:03 -0500

    Integrate with heroku server.

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -3,18 +3,12 @@ angular.module('SteroidsApplication', [
 ])
 .controller('IndexController', function($scope, $http, supersonic) {
 
-  $scope.allposts = {}
+  $scope.allposts = {};
   $scope.navbarTitle = "Hammock";
 
-  $scope.tempFriend = {
-    name: 'Joon',
-    location: 'Lakefill',
-    time: 120
-  };
-
-  $http.get("www.herokuapp.com/allposts")
+  $http.get("https://tree2hammock.herokuapp.com/getLiveData")
   	.success(function(data) {
-  		$scope.allposts = data;
+      $scope.allposts = data;
     }).error(function(err) {
   		console.log("The error is: " + err);
   	});
