79e4d1068aff7f0a4821858ed928a9897771241f
Joon Park <JoonPark2017@u.northwestern.edu>
2016-09-25 22:54:29 -0500

    Add initial infobox functionality.

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -19,8 +19,42 @@ angular.module('SteroidsApplication', [
     "#CA7C1B",
     "#D85820"
   ];
+  $scope.boxInfo = function(index, data) {
+    /*
+    const options = {
+      message: "<div>Div 1</div><div>div 2 </div>",
+      buttonLabels: ["I'll join you!"]
+    };
 
-  $http.get("https://tree2hammock.herokuapp.com/getLiveData")
+    supersonic.ui.dialog.confirm(data.name + " is at " + data.location, options).then(function(index) {
+      if (index == 0) {
+        supersonic.logger.log("I'll join you!");
+      }
+    });
+    */
+
+    const infobox = $('<div></div>');
+    infobox.addClass('info-box padding-vertical');
+
+    const time = $('<p></p>');
+    time.append("from " + data.start_time + " until " + data.end_time);
+    infobox.append(time);
+
+    const info = $('<p></p>');
+    info.append("Activity: " + data.event);
+    infobox.append(info);
+
+    const button = $('<button></button>');
+    button.addClass('button button-outline button-positive');
+    button.css('float', 'right');
+    button.append('I\'ll join you!');
+    infobox.append(button);
+
+    const selectedBox = $('#box-' + index);
+    selectedBox.after(infobox);
+  };
+
+  $http.get("https://tree2hammock.herokuapp.com/getLiveEvents")
   	.success(function(data) {
       $scope.allposts = data;
     }).error(function(err) {
