d045285cadab7c4583d2f125383826d5add88ec9
Joon Park <JoonPark2017@u.northwestern.edu>
2016-09-28 23:20:13 -0500

    Reload mainfeed whenever form is submitted.

--- app/hang/scripts/IndexController.js
+++ app/hang/scripts/IndexController.js
@@ -1,14 +1,15 @@
 angular
   .module('hang')
-  .controller('IndexController', function($scope, supersonic) {
+  .controller('IndexController', function($scope, supersonic, $http) {
     $scope.sendEventForm = function() {
       $http.post("http://tree2hammock.herokuapp.com/addNewEvent", JSON.stringify($scope.newEvent))
-        .success(function(res){
+        .success(function(res) {
           $scope.newEvent = $scope.eventFormat;
-          $scope.successmsg = "New Event Created"
-          $scope.getEvents();
-        }).error(function(err){
-          $scope.tempCreate = "Not created"
+          $scope.successmsg = "New Event Created";
+
+          supersonic.data.channel('eventAdded').publish({event: "added"});
+        }).error(function(err) {
+          $scope.tempCreate = "Not created";
         })
     };
   });
--- app/mainfeed/scripts/IndexController.js
+++ app/mainfeed/scripts/IndexController.js
@@ -1,63 +1,68 @@
 angular.module('mainfeed').controller('IndexController', function($scope, supersonic, $http) {
   supersonic.ui.tabs.hide();
-    // $scope.allposts = {}
-    $scope.navbarTitle = "Hammock";
-    $scope.eventFormat = {data : {name:"", user_id:"", location: "", start_time:"", end_time:"", "event":""}};
-    $scope.newEvent    = {data : {name:"", user_id:"", location: "", start_time:"", end_time:"", theEvent:""}};
-    $scope.boxColors = [
-      "#58B947",
-      "#7FCECD",
-      "#5091CD",
-      "#EDE93B",
-      "#FFC520",
-      "#EF553F",
-      "#008656",
-      "#007FA4",
-      "#0D2D6C",
-      "#D9C826",
-      "#CA7C1B",
-      "#D85820"
-    ];
 
-    $scope.boxInfo = function(index, data) {
-      const selectedBox = $('#box-' + index);
-      const open = selectedBox.attr('data-info-open');
-      if (open === 'false') {
-        const infobox = $('<div></div>');
-        infobox.addClass('info-box padding-vertical');
+  $scope.navbarTitle = "Hammock";
+  $scope.eventFormat = {data : {name:"", user_id:"", location: "", start_time:"", end_time:"", "event":""}};
+  $scope.newEvent    = {data : {name:"", user_id:"", location: "", start_time:"", end_time:"", theEvent:""}};
+  $scope.boxColors = [
+    "#58B947",
+    "#7FCECD",
+    "#5091CD",
+    "#EDE93B",
+    "#FFC520",
+    "#EF553F",
+    "#008656",
+    "#007FA4",
+    "#0D2D6C",
+    "#D9C826",
+    "#CA7C1B",
+    "#D85820"
+  ];
 
-        const time = $('<p></p>');
-        time.append("from " + data.start_time + " until " + data.end_time);
-        infobox.append(time);
+  $scope.boxInfo = function(index, data) {
+    const selectedBox = $('#box-' + index);
+    const open = selectedBox.attr('data-info-open');
+    if (open === 'false') {
+      const infobox = $('<div></div>');
+      infobox.addClass('info-box padding-vertical');
 
-        const info = $('<p></p>');
-        info.append("Activity: " + data.event);
-        infobox.append(info);
+      const time = $('<p></p>');
+      time.append("from " + data.start_time + " until " + data.end_time);
+      infobox.append(time);
 
-        const button = $('<button></button>');
-        button.addClass('button button-outline button-positive');
-        button.css('float', 'right');
-        button.append('I\'ll join you!');
-        infobox.append(button);
+      const info = $('<p></p>');
+      info.append("Activity: " + data.event);
+      infobox.append(info);
 
-        selectedBox.after(infobox);
+      const button = $('<button></button>');
+      button.addClass('button button-outline button-positive');
+      button.css('float', 'right');
+      button.append('I\'ll join you!');
+      infobox.append(button);
 
-        selectedBox.attr('data-info-open', 'true');
-      } else if (open === 'true') {
-        selectedBox.next().remove();
+      selectedBox.after(infobox);
 
-        selectedBox.attr('data-info-open', 'false');
-      }
-    };
+      selectedBox.attr('data-info-open', 'true');
+    } else if (open === 'true') {
+      selectedBox.next().remove();
 
-    $scope.getEvents = function() {
-      $http.get("http://tree2hammock.herokuapp.com/getLiveEvents")
-        .success(function(data) {
-          $scope.allposts = data;
-        })
-        .error(function(err){
-          // console.log("The error is: " + err);
-          $scope.allposts = JSON.stringify(err);
-        });
-    };
+      selectedBox.attr('data-info-open', 'false');
+    }
+  };
+
+  $scope.getEvents = function() {
+    $http.get("http://tree2hammock.herokuapp.com/getLiveEvents").success(function(data) {
+      $scope.allposts = data;
+    }).error(function(err){
+      // console.log("The error is: " + err);
+      $scope.allposts = JSON.stringify(err);
+    });
+  };
+
+  supersonic.data.channel('eventAdded').subscribe(function(message) {
+    if (message.event == 'added') {
+      $scope.getEvents();
+    }
   });
+
+});
