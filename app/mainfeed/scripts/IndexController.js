3fd89c8929122459ae048a4d7616f54d4ef30eaf
Collin Pham <collinpham2018@u.northwestern.edu>
2016-09-28 23:15:11 -0500

    Updated Event Info Box

--- app/mainfeed/scripts/IndexController.js
+++ app/mainfeed/scripts/IndexController.js
@@ -5,51 +5,57 @@ angular.module('mainfeed').controller('IndexController', function($scope, supers
     $scope.eventFormat = {data : {name:"", user_id:"", location: "", start_time:"", end_time:"", "event":""}};
     $scope.newEvent    = {data : {name:"", user_id:"", location: "", start_time:"", end_time:"", theEvent:""}};
     $scope.boxColors = [
-      "#58B947",
       "#7FCECD",
       "#5091CD",
       "#EDE93B",
       "#FFC520",
       "#EF553F",
-      "#008656",
       "#007FA4",
       "#0D2D6C",
       "#D9C826",
       "#CA7C1B",
       "#D85820"
     ];
-
-    $scope.boxInfo = function(index, data) {
-      const selectedBox = $('#box-' + index);
-      const open = selectedBox.attr('data-info-open');
-      if (open === 'false') {
-        const infobox = $('<div></div>');
-        infobox.addClass('info-box padding-vertical');
-
-        const time = $('<p></p>');
-        time.append("from " + data.start_time + " until " + data.end_time);
-        infobox.append(time);
-
-        const info = $('<p></p>');
-        info.append("Activity: " + data.event);
-        infobox.append(info);
-
-        const button = $('<button></button>');
-        button.addClass('button button-outline button-positive');
-        button.css('float', 'right');
-        button.append('I\'ll join you!');
-        infobox.append(button);
-
-        selectedBox.after(infobox);
-
-        selectedBox.attr('data-info-open', 'true');
-      } else if (open === 'true') {
-        selectedBox.next().remove();
-
-        selectedBox.attr('data-info-open', 'false');
+    $scope.toggleEventInfo = function (index, data) {
+      var state = document.querySelector('#box-info-'+index).style.display
+      steroids.logger.log(state)
+      if (state === 'none' || state === '') {
+        document.querySelector('#box-info-'+index).style.display = 'block'
+      } else {
+        document.querySelector('#box-info-'+index).style.display = 'none'
       }
+    }
+    // $scope.boxInfo = function(index, data) {
+    //   const selectedBox = $('#box-' + index);
+    //   const open = selectedBox.attr('data-info-open');
+    //   if (open === 'false') {
+    //     const infobox = $('<div></div>');
+    //     infobox.addClass('info-box padding-vertical');
 
-    };
+    //     const time = $('<p></p>');
+    //     time.append("from " + data.start_time + " until " + data.end_time);
+    //     infobox.append(time);
+
+    //     const info = $('<p></p>');
+    //     info.append("Activity: " + data.event);
+    //     infobox.append(info);
+
+    //     const button = $('<button></button>');
+    //     button.addClass('button button-outline button-positive');
+    //     button.css('float', 'right');
+    //     button.append('I\'ll join you!');
+    //     infobox.append(button);
+
+    //     selectedBox.after(infobox);
+
+    //     selectedBox.attr('data-info-open', 'true');
+    //   } else if (open === 'true') {
+    //     selectedBox.next().remove();
+
+    //     selectedBox.attr('data-info-open', 'false');
+    //   }
+
+    // };
 
     $scope.getEvents = function() {
       $http.get("http://tree2hammock.herokuapp.com/getLiveEvents")
