1052dfdd764889a29892c421ad639fabcf7661e5
Joon Park <JoonPark2017@u.northwestern.edu>
2016-10-05 21:06:45 -0500

    Implement confirm dialog and return user to previous view.

--- app/mainfeed/scripts/IndexController.js
+++ app/mainfeed/scripts/IndexController.js
@@ -2,8 +2,6 @@ angular.module('mainfeed').controller('IndexController', function($scope, supers
   supersonic.ui.tabs.hide();
 
   $scope.navbarTitle = "Hammock";
-  $scope.eventFormat = {data : {name:"", user_id:"", location: "", start_time:"", end_time:"", "event":""}};
-  $scope.newEvent    = {data : {name:"", user_id:"", location: "", start_time:"", end_time:"", theEvent:""}};
   $scope.boxColors = [
     "#7FCECD",
     "#5091CD",
@@ -18,7 +16,6 @@ angular.module('mainfeed').controller('IndexController', function($scope, supers
   ];
   $scope.toggleEventInfo = function (index, data) {
     var state = document.querySelector('#box-info-'+index).style.display
-    steroids.logger.log(state)
     if (state === 'none' || state === '') {
       document.querySelector('#box-info-'+index).style.display = 'block'
     } else {
