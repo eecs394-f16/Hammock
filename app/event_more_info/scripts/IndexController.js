55df556ec1736d97a6de4b22caabbb5777d998ff
Joon Park <JoonPark2017@u.northwestern.edu>
2016-10-25 02:40:19 -0500

    Add I'm going button

--- app/event_more_info/scripts/IndexController.js
+++ app/event_more_info/scripts/IndexController.js
@@ -1,5 +1,16 @@
 angular
   .module('event_more_info')
   .controller('IndexController', function($scope, supersonic) {
-    // Controller functionality here
+    $scope.going = function() {
+      var options = {
+        title: "Name",
+        buttonLabels: ["Submit"],
+        defaultText: "Enter your name"
+      };
+
+      supersonic.ui.dialog.prompt("I'm going!", options).then(function(result) {
+        supersonic.logger.log("User clicked button number " + result.buttonIndex + " with text " + result.input);
+        // TODO: Send a post request with result.input
+      });
+    };
   });
